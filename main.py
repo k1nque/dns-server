cb8879c994404a047975bd24295dba9c0de5370e
k1nque <artkashaev@gmail.com>
2022-03-29 23:40:07 +0500

    something wrong, but parcing req/resp almost done

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ ip = "127.0.0.1"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
 
-def load_zones():
-    json_zone = {}
-    zone_files = glob.glob("zones/*.zone")
 
+def load_zones():
+    json_zone = dict()
+    zone_files = glob.glob("zones/*.zone")
+    # print(zone_files)
     for zone in zone_files:
         with open(zone) as zonedata:
             data = json.load(zonedata)
@@ -20,12 +21,12 @@ def load_zones():
 
     return json_zone
 
+
 zone_data = load_zones()
 
 
 def parse_flags(flags):
     fs_byte = bytes(flags[:1])
-    sc_byte = bytes(flags[1:])
 
     rflags = ""
 
@@ -49,11 +50,12 @@ def get_question_domain(data):
     domain_string = ""
     domain_parts = []
     x = 0
-    y = 0
+    qti = 0
 
     for byte in data:
         if state:
-            domain_string += chr(byte)
+            if byte != 0:
+                domain_string += chr(byte)
             x += 1
             if x == expected_length:
                 domain_parts.append(domain_string)
@@ -66,44 +68,98 @@ def get_question_domain(data):
         else:
             state = True
             expected_length = byte
-        y += 1
-
-    return get_recs('.'.join(domain_parts), data[y + 1:y + 3])
+        qti += 1
+        # print(qti)
+    # print(domain_parts)
+    # print(data[qti:qti + 2])
+    # print(data)
+    # print(qti)
+    return '.'.join(domain_parts[:-1]) + '.', data[qti:qti + 2]
 
 
 def get_zone(domain):
     global zone_data
-    zone_name = domain + '.'
+    zone_name = domain
+    # print(zone_data)
     return zone_data[zone_name]
 
 
-def get_recs(domain, question_type):
-    at = ''
+def get_recs(data):
+    domain, question_type = get_question_domain(data)
     qt = ''
     if question_type == b'\x00\x01':
         qt = 'a'
 
     zone = get_zone(domain)
-    print(zone)
-    return zone, qt, domain
+    # print(zone, qt, domain)
+    return zone[qt], qt, domain
+
+
+def build_question(domain_parts, rectype):
+    qbytes = b''
+
+    for part in domain_parts:
+        length = len(part)
+        qbytes += bytes([length])
+
+        for char in part:
+            qbytes += ord(char).to_bytes(1, byteorder="big")
+
+    if rectype == 'a':
+        qbytes += (1).to_bytes(2, byteorder="big")
+
+    qbytes += (1).to_bytes(2, byteorder="big")
+
+    return qbytes
+
+
+def rec2bytes(domain_name, rectype, recttl, recvalue):
+    rbytes = b'\xc0\x0c'
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([1])
+    rbytes = rbytes + bytes([0]) + bytes([1])
+    rbytes += int(recttl).to_bytes(4, byteorder="big")
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([4])
+
+        for part in recvalue.split('.'):
+            rbytes += bytes([int(part)])
+
+    return rbytes
 
 
 def build_response(data):
     # Transaction ID
     TransactionID = data[:2]
-    TID = ""
-    for byte in TransactionID:
-        TID += hex(byte)[2:]
 
     # Flags
     Flags = parse_flags(data[2:4])
 
     # Question Count
-    QDCOUNT = b = '\x00\x01'
+    QDCOUNT = b'\x00\x01'
 
     # Answer Count
-    get_question_domain(data[12:])
+    ANCOUNT = (len(get_recs(data[12:])[0])).to_bytes(2, byteorder='big')
 
+    # Nameserver Count
+    NSCOUNT = (0).to_bytes(2, byteorder="big")
+
+    # Additional Count
+    ARCOUNT = (0).to_bytes(2, byteorder="big")
+    # print(type(TransactionID), type(Flags), type(QDCOUNT), type(ANCOUNT), type(NSCOUNT), type(ARCOUNT))
+    dns_header = TransactionID + Flags + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT
+    dns_body = b''
+
+    records, rectype, domain_name = get_recs(data[12:])
+
+    dns_question = build_question(domain_name, rectype)
+
+    for record in records:
+        dns_body += rec2bytes(domain_name, rectype, record["ttl"], record["value"])
+
+    return dns_header + dns_question + dns_body
 
 while True:
     data, addr = sock.recvfrom(512)
